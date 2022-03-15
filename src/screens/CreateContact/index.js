0486327caffb22dd18ab00dd3e226eb557adfdb6
Ayo-David Ajayi <ayodavid.ajayi@gmail.com>
2022-03-15 21:01:55 +0100

    upload image to firebase

--- src/screens/CreateContact/index.js
+++ src/screens/CreateContact/index.js
@@ -4,6 +4,7 @@ import CreateContactComponent from '../../components/CreateContactComponent';
 import { CONTACT_LIST } from '../../constants/routeNames';
 import createContact from '../../context/actions/contacts/createContact';
 import useDataLayer from '../../context/Provider';
+import uploadImage from '../../helpers/uploadImage';
 
 const CreateContact = () => {
     const { navigate } = useNavigation()
@@ -15,6 +16,7 @@ const CreateContact = () => {
     const [form, setForm] = useState({})
     const sheetRef = useRef(null)
     const [localFile, setLocalFile] = useState(null);
+    const [isUploading, setIsUploading] = useState(false);
 
     const onChangeHandler = ({ name, val }) => {
         setForm({
@@ -40,9 +42,22 @@ const CreateContact = () => {
 
     const submitForm = () => {
         console.log('form:', form)
-        createContact(form)(contactsDispatch)(() => {
-            navigate(CONTACT_LIST)
-        })
+        if (localFile?.size) {
+            setIsUploading(true)
+            uploadImage(localFile)((url) => {
+                setIsUploading(false)
+                createContact({ ...form, contact_picture: url })(contactsDispatch)(() => {
+                    navigate(CONTACT_LIST)
+                })
+            })(error => {
+                console.log('error with image upload ', error)
+                setIsUploading(false)
+            })
+        } else {
+            createContact(form)(contactsDispatch)(() => {
+                navigate(CONTACT_LIST)
+            })
+        }
     }
     const toggleSwitch = () => {
         setForm({ ...form, 'is_favorite': !form.is_favorite })
@@ -53,7 +68,7 @@ const CreateContact = () => {
             onChangeHandler={onChangeHandler}
             submitForm={submitForm}
             error={error}
-            loading={loading}
+            loading={isUploading}
             setForm={setForm}
             form={form}
             toggleSwitch={toggleSwitch}
