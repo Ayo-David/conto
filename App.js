3d4d49f6481989b42a6c1de448bdd38c317e162e
Ayo-David Ajayi <ayodavid.ajayi@gmail.com>
2022-05-25 13:26:55 +0100

    add logbox to ignore rn gesture

--- App.js
+++ App.js
@@ -8,8 +8,13 @@
 import 'react-native-gesture-handler';
 import React from 'react';
 import AppNavContainer from './src/navigations';
+import { LogBox } from 'react-native';
 import { GlobalProvider } from './src/context/Provider';
 
+
+LogBox.ignoreLogs([
+  "[react-native-gesture-handler] Seems like you\'re using an old API with gesture components, check out new Gestures system!",
+]);
 
 const App = () => {
 
